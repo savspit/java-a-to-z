46a04491a67f02c60c9ffb79a9ccdffb9c7e83df
Anton Shestakov <savspit@gmail.com>
2016-04-22 15:11:25 +0300

    add structure

---
+++ Part II. Chapter 2. Inkapsulyaciya/Comment.java
@@ -0,0 +1,11 @@
+package ru.shestakov.models;
+
+public class Comment extends Item {
+	
+	String text;
+	
+	public Comment(String text) {
+		this.text = text;
+	}
+	
+}
---
+++ Part II. Chapter 2. Inkapsulyaciya/StartUI.java
@@ -0,0 +1,65 @@
+package ru.shestakov.start;
+
+import ru.shestakov.models.*;
+
+public class StartUI {
+	
+	public static void main(String[] args) {
+		
+		Tracker tracker = new Tracker();
+		
+		// add
+		
+		Task task1 = new Task("first task", "first desc", System.nanoTime());
+		tracker.add(task1);
+		
+		Task task2 = new Task("second task", "second desc", System.nanoTime());
+		tracker.add(task2);
+		
+		Task task3 = new Task("third task", "third desc", System.nanoTime());
+		tracker.add(task3);
+		
+		Task task4 = new Task("forth task", "forth desc", System.nanoTime());
+		tracker.add(task4);
+		
+		// update
+		
+		tracker.update(task2, "updated task", "updated desc");
+		
+		// delete
+		
+		tracker.delete(task1);
+		
+		// find by description
+		
+		System.out.println("founded task is: " + tracker.findByName("third task"));
+		
+		// find by name
+		
+		System.out.println("founded task is: " + tracker.findByDesc("forth desc"));
+		
+		// find by create
+		
+		System.out.println("founded task is: " + tracker.findByCreate(271545279656360L));
+		
+		// add comment
+		
+		Comment comment = new Comment("Some kind of comment");
+		tracker.addComment(task3, comment);
+		for (Comment nextComment : task3.getComments()) {
+			if(nextComment != null) {
+				System.out.println("task comment is: " + nextComment);
+			}
+		}
+				
+		// print all
+		
+		for (Item item : tracker.getAll()) {
+			if(item != null) {
+				System.out.println(item.getName());
+			}
+		}
+		
+	}
+	
+}
