d861afe78298768ba4144982b2bfc6fc92f617f9
Anton Shestakov <savspit@gmail.com>
2016-04-28 16:02:24 +0300

    add structure

---
+++ Part II. Chapter 3. Polimorfizm/Tracker/src/main/java/ru/shestakov/models/Comment.java
@@ -0,0 +1,21 @@
+package ru.shestakov.models;
+
+public class Comment extends Item {
+
+    private String itemId;
+    public String text;
+
+    public Comment(String itemId, String text) {
+        this.itemId = itemId;
+        this.text = text;
+    }
+
+    public String getItemId() {
+        return this.itemId;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+}
+
---
+++ Part II. Chapter 3. Polimorfizm/Tracker/src/main/java/ru/shestakov/start/ConsoleInput.java
@@ -0,0 +1,12 @@
+package ru.shestakov.start;
+
+import java.util.*;
+
+public class ConsoleInput implements Input {
+    private Scanner scanner = new Scanner(System.in);
+
+    public String ask(String question) {
+        System.out.print(question);
+        return scanner.nextLine();
+    }
+}
