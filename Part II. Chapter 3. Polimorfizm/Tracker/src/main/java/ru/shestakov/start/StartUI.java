2796587efdcb3c347bc7d67a5e35ef0ca4082b33
Anton Shestakov <savspit@gmail.com>
2016-04-28 16:15:46 +0300

    add structure

--- Part II. Chapter 3. Polimorfizm/Tracker/src/main/java/ru/shestakov/start/StartUI.java
+++ Part II. Chapter 3. Polimorfizm/Tracker/src/main/java/ru/shestakov/start/StartUI.java
@@ -11,7 +11,7 @@ public class StartUI {
 
     public void init(Tracker tracker) {
 
-        String result = input.ask("Please, enter the action (1-add, 2-update, 3-delete, 4-add comment, 5-find by name, 6-find by description, 7-find by date, 8-print all): ");
+        String result = input.ask("Please, enter the action (1-add, 2-update, 3-delete, 4-add comment, 5-find by name, 6-find by description, 7-find by date, 8-print all, 9-exit): ");
 
         if (result.equals("1")) {
             new StartUI(input).addItem(tracker);
@@ -45,12 +45,10 @@ public class StartUI {
             new StartUI(input).printAll(tracker);
         }
 
-        init(tracker);
+        if (!result.equals("9")) {
+            init(tracker);
+        }
 
-        /*
-        String name = input.ask("Please, enter the task`s name: ");
-        Tracker tracker = new Tracker();
-        tracker.add(new Task(name, "first desc", System.nanoTime()));*/
     }
 
     public void addItem(Tracker tracker) {
@@ -60,12 +58,7 @@ public class StartUI {
         String create = input.ask("Please, enter the new task`s date to add: ");
         tracker.add(new Task(name, desc, Long.parseLong(create)));
 
-        /*
-        for (Item item : tracker.getAll()) {
-            System.out.println(item.getName() + " " + item.getId());
-        }*/
-
-    }
+     }
 
     public void updateItem(Tracker tracker) {
 
@@ -85,11 +78,6 @@ public class StartUI {
             System.out.println("This id does not exists");
         }
 
-        /*
-        for (Item item : tracker.getAll()) {
-            System.out.println(item.getName() + " " + item.getId());
-        }*/
-
     }
 
     public void deleteItem(Tracker tracker) {
@@ -105,11 +93,6 @@ public class StartUI {
         } else {
             System.out.println("This id does not exists");
         }
-
-        /*
-        for (Item item : tracker.getAll()) {
-            System.out.println(item.getName() + " " + item.getId());
-        }*/
 
     }
 
@@ -128,12 +111,6 @@ public class StartUI {
             System.out.println("This id does not exists");
         }
 
-        /*
-        for (Comment nextComment : tracker.findById(id).getComments()) {
-            if(nextComment != null) {
-                System.out.println("task comment is: " + nextComment);
-            }
-        }*/
     }
 
     public void findByName(Tracker tracker) {
@@ -144,12 +121,7 @@ public class StartUI {
 
         Item[] result = tracker.findBy(filter);
 
-        /*
-        for (Item item : result) {
-            System.out.println(item.getId() + " " + item.getName());
-        }*/
-
-    }
+     }
 
     public void findByDescription(Tracker tracker) {
 
@@ -158,11 +130,6 @@ public class StartUI {
         Filter filter = new FilterByDescription(desc);
 
         Item[] result = tracker.findBy(filter);
-
-        /*
-        for (Item item : result) {
-            System.out.println(item.getId() + " " + item.getDescription());
-        }*/
 
     }
 
@@ -174,12 +141,7 @@ public class StartUI {
 
         Item[] result = tracker.findBy(filter);
 
-        /*
-        for (Item item : result) {
-            System.out.println(item.getId() + " " + item.getCreate());
-        }*/
-
-    }
+     }
 
     public void printAll(Tracker tracker) {
 
@@ -204,80 +166,6 @@ public class StartUI {
 
         new StartUI(input).init(tracker);
 
-        //Input input = new ConsoleInput();
-        //new StartUI(input).init();
-
-
-
-        /*
-        Tracker tracker = new Tracker();
-
-        // add
-
-        Task task1 = new Task("first task", "first desc", System.nanoTime());
-        tracker.add(task1);
-
-        Task task2 = new Task("second task", "second desc", System.nanoTime());
-        tracker.add(task2);
-
-        Task task3 = new Task("third task", "third desc", System.nanoTime());
-        tracker.add(task3);
-
-        Task task4 = new Task("forth task", "forth desc", System.nanoTime());
-        tracker.add(task4);
-
-        // update
-
-        Task task5 = new Task("fifth task", "fifth desc", System.nanoTime());
-        task5.setId(task2.getId());
-        tracker.update(task5);
-
-        // delete
-
-        tracker.delete(task1);
-
-        // find by name
-
-        Task task6 = new Task("sixth task", "sixth desc", System.nanoTime());
-        tracker.add(task6);
-        Filter filter1 = new FilterByName(task6.getName());
-
-        System.out.println("founded array task by name is: " + tracker.findBy(filter1));
-
-
-        // find by description
-
-        Task task7 = new Task("seventh task", "seventh desc", System.nanoTime());
-        tracker.add(task7);
-        Filter filter2 = new FilterByDescription(task7.getDescription());
-
-        System.out.println("founded array task by description is: " + tracker.findBy(filter2));
-
-        // find by create
-        Task task8 = new Task("eighth task", "eighth desc", 271545279656360L);
-        tracker.add(task8);
-        Filter filter3 = new FilterByDescription(task8.getDescription());
-
-        System.out.println("founded array task by create is: " + tracker.findBy(filter3));
-
-        // add comment
-
-        Comment comment = new Comment(task3.getId(), "Some kind of comment");
-        tracker.addComment(comment);
-        for (Comment nextComment : task3.getComments()) {
-            if(nextComment != null) {
-                System.out.println("task comment is: " + nextComment);
-            }
-        }
-
-        // print all
-
-        for (Item item : tracker.getAll()) {
-            if(item != null) {
-                System.out.println(item.getName());
-            }
-        }*/
-
-    }
+     }
 
 }
