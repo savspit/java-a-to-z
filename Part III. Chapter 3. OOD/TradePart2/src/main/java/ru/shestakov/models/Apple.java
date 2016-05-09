3b66d243177d5b344757632d7e620aa4d67ff5db
Anton Shestakov <savspit@gmail.com>
2016-05-09 17:37:27 +0300

    add structure with folder

---
+++ Part III. Chapter 3. OOD/TradePart2/src/main/java/ru/shestakov/models/Apple.java
@@ -0,0 +1,8 @@
+package ru.shestakov.models;
+
+public class Apple extends Food {
+
+    public Apple(String name, long expaireDate) {
+        super(name, expaireDate);
+    }
+}
---
+++ Part III. Chapter 3. OOD/TradePart2/src/main/java/ru/shestakov/models/Warehouse.java
@@ -0,0 +1,7 @@
+package ru.shestakov.models;
+
+public class Warehouse extends Storage {
+
+    public Food[] storage = new Food[100];
+
+}
