50a7c74ddac9241baca50d8376cef76c7cc22f6d
Anton Shestakov <savspit@gmail.com>
2016-05-09 17:35:04 +0300

    add structure with folder

---
+++ Part III. Chapter 3. OOD/TradePart1/src/main/java/ru/shestakov/models/Storage.java
@@ -0,0 +1,16 @@
+package ru.shestakov.models;
+
+public abstract class Storage {
+
+    public String name;
+    public Food storage[];
+
+    public void addFood(Food food) {
+        for (int x = 0; x < this.storage.length; x++) {
+            if(storage[x] != null) {
+                storage[x] = food;
+                break;
+            }
+        }
+    }
+}
