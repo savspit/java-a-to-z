796337de219234e289c57d99991771206bad3d4c
Anton Shestakov <savspit@gmail.com>
2016-05-09 19:29:29 +0300

    delete common with folder

--- Part III. Chapter 3. LSP/TradePart2/src/main/java/ru/shestakov/models/ReproductStorage.java
+++
@@ -1,7 +0,0 @@
-package ru.shestakov.models;
-
-public class ReproductStorage extends Storage {
-
-    public Food[] storage = new Food[100];
-
-}
--- Part III. Chapter 3. LSP/TradePart2/src/main/java/ru/shestakov/models/Warehouse.java
+++
@@ -1,7 +0,0 @@
-package ru.shestakov.models;
-
-public class Warehouse extends Storage {
-
-    public Food[] storage = new Food[100];
-
-}
