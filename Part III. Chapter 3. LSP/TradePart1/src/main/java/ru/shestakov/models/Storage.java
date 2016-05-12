ad371fa7cb10d3243efa5edf38a665d2fee3dfaf
Anton Shestakov <savspit@gmail.com>
2016-05-12 23:38:35 +0300

    add structure with folder

--- Part III. Chapter 3. LSP/TradePart1/src/main/java/ru/shestakov/models/Storage.java
+++ Part III. Chapter 3. LSP/TradePart1/src/main/java/ru/shestakov/models/Storage.java
@@ -5,12 +5,7 @@ public abstract class Storage {
     public String name;
     public Food storage[];
 
-    public void addFood(Food food) {
-        for (int x = 0; x < this.storage.length; x++) {
-            if(storage[x] != null) {
-                storage[x] = food;
-                break;
-            }
-        }
-    }
+    public void addFood(Food food) { }
+
+    public boolean appropriate(Food food) { return false; }
 }
