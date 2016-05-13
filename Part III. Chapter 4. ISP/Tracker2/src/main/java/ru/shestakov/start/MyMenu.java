e9b4024b4d34ca5605a8947a6bf07410c227bc4b
Anton Shestakov <savspit@gmail.com>
2016-05-13 10:29:05 +0300

    delete common with folder

--- Part III. Chapter 4. ISP/Tracker2/src/main/java/ru/shestakov/start/MyMenu.java
+++
@@ -1,7 +0,0 @@
-package ru.shestakov.start;
-
-/**
- * Created by user on 10.05.2016.
- */
-public class MyMenu {
-}
--- Part III. Chapter 4. ISP/Tracker2/src/main/java/ru/shestakov/start/SimpleInput.java
+++
@@ -1,5 +0,0 @@
-package ru.shestakov.start;
-
-public interface SimpleInput {
-    String ask(String question);
-}
