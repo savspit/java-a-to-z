7c92852e3db57925ce48612aaee4130051bd1356
Anton Shestakov <savspit@gmail.com>
2016-05-18 15:00:38 +0300

    add structure with folder

--- Part III. Chapter 6. TDD/Tracker/src/main/java/ru/shestakov/templates/SimpleGenerator.java
+++ Part III. Chapter 6. TDD/Tracker/src/main/java/ru/shestakov/templates/SimpleGenerator.java
@@ -1,27 +1,79 @@
 package ru.shestakov.templates;
 
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class SimpleGenerator implements Template {
+public class SimpleGenerator implements Template{
 
-    public String generate(String template, Object[] data) {
+    public String generate(String template, Map<String,String> data) {
+
+        String error = getError(template, data);
+        if(error != null) { return error; }
 
         Pattern p = Pattern.compile("\\$\\{(\\w+)\\}");
         Matcher m = p.matcher(template);
 
         StringBuffer sb = new StringBuffer();
-        int level = 0;
         while (m.find()) {
-            try {
-                m.appendReplacement(sb, String.valueOf(data[level++]));
-            } catch (ArrayIndexOutOfBoundsException aie) {
-                return null;
-            }
+            m.appendReplacement(sb, data.get(m.group(1)));
         }
 
         m.appendTail(sb);
 
         return sb.toString();
     }
+
+    public String getError(String template, Map<String,String> data) {
+        if(isEmptyDataError(data)) { return "e001"; }
+        if(isEmptyTextError(template)) { return "e002"; }
+        if(isKeysInDataLessKeysInText(template, data)) { return "e003"; }
+        if(isKeysInTextLessKeysInData(template, data)) { return "e004"; }
+        return null;
+    }
+
+    public boolean isEmptyDataError(Map<String,String> data) {
+        return data.size() == 0;
+    }
+
+    public boolean isEmptyTextError(String template) {
+        Pattern p = Pattern.compile("\\$\\{(\\w+)\\}");
+        Matcher m = p.matcher(template);
+
+        StringBuffer sb = new StringBuffer();
+
+        return !(m.find());
+    }
+
+    public boolean isKeysInDataLessKeysInText(String template, Map<String,String> data) {
+
+        Pattern p = Pattern.compile("\\$\\{(\\w+)\\}");
+        Matcher m = p.matcher(template);
+
+        StringBuffer sb = new StringBuffer();
+
+        int counter = 0;
+        while (m.find()) {
+            counter++;
+        }
+
+        return counter > data.size();
+    }
+
+    public boolean isKeysInTextLessKeysInData(String template, Map<String,String> data) {
+
+        Pattern p = Pattern.compile("\\$\\{(\\w+)\\}");
+        Matcher m = p.matcher(template);
+
+        StringBuffer sb = new StringBuffer();
+
+        int counter = 0;
+        while (m.find()) {
+            counter++;
+        }
+
+        return counter < data.size();
+    }
+
+
 }
--- Part III. Chapter 6. TDD/Tracker/src/test/java/ru/shestakov/templates/TemplateTest.java
+++ Part III. Chapter 6. TDD/Tracker/src/test/java/ru/shestakov/templates/TemplateTest.java
@@ -3,6 +3,9 @@ package ru.shestakov.templates;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -14,7 +17,8 @@ public class TemplateTest {
         // assign
         SimpleGenerator template = new SimpleGenerator();
         String text = "Hello, ${name}.";
-        String[] data = new String[] {"Petr"};
+        Map<String,String> data = new HashMap<String, String>();
+        data.put("name", "Petr");
         String checked = "Hello, Petr.";
 
         // act
@@ -25,18 +29,74 @@ public class TemplateTest {
     }
 
     @Test
-    public void whenTakeTestWithSmallDataShouldReturnNull() {
+    public void whenTakeTestWithNoKeysInDataShouldReturnError() {
 
         // assign
         SimpleGenerator template = new SimpleGenerator();
         String text = "Hello, ${name}. Hello, ${name}.";
-        String[] data = new String[] {"Petr"};
-        String checked = null;
+        Map<String,String> data = new HashMap<String, String>();
 
         // act
         String result = template.generate(text, data);
 
         // action
+        String checked = "e001";
         Assert.assertThat(result, is(checked));
+
+    }
+
+    @Test
+    public void whenTakeTestWithNoKeysInTextShouldReturnError() {
+
+        // assign
+        SimpleGenerator template = new SimpleGenerator();
+        String text = "Hello!.";
+        Map<String,String> data = new HashMap<String, String>();
+        data.put("namet", "Petr");
+
+        // act
+        String result = template.generate(text, data);
+
+        // action
+        String checked = "e002";
+        Assert.assertThat(result, is(checked));
+
+    }
+
+    @Test
+    public void whenTakeTestWithKeysInDataLessKeysInTextShouldReturnError() {
+
+        // assign
+        SimpleGenerator template = new SimpleGenerator();
+        String text = "Hello, ${name}, ${surname}.";
+        Map<String,String> data = new HashMap<String, String>();
+        data.put("name", "Petr");
+
+        // act
+        String result = template.generate(text, data);
+
+        // action
+        String checked = "e003";
+        Assert.assertThat(result, is(checked));
+
+    }
+
+    @Test
+    public void whenTakeTestWithKeysInTextLessKeysInDataShouldReturnError() {
+
+        // assign
+        SimpleGenerator template = new SimpleGenerator();
+        String text = "Hello, ${name}.";
+        Map<String,String> data = new HashMap<String, String>();
+        data.put("name", "Petr");
+        data.put("surname", "Parsentev");
+
+        // act
+        String result = template.generate(text, data);
+
+        // action
+        String checked = "e004";
+        Assert.assertThat(result, is(checked));
+
     }
 }
