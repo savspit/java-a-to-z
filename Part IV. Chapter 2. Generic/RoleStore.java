99b30b64df81937f907406540994d1cbdae3df7c
Anton Shestakov <savspit@gmail.com>
2016-06-07 08:26:28 +0300

    add folder with structure

--- Part IV. Chapter 2. Generic/RoleStore.java
+++ Part IV. Chapter 2. Generic/RoleStore.java
@@ -2,10 +2,26 @@ package ru.shestakov.services;
 
 public class RoleStore<Role> {
 
-    SimpleArray[] sa;
+    SimpleArray<Role> sa;
 
     public RoleStore(int size) {
-        this.sa = new SimpleArray[size];
+        this.sa = new SimpleArray<Role>(size);
+    }
+
+    public void add(Role role) {
+        this.sa.add(role);
+    }
+
+    public void update(int index, Role role) {
+        this.sa.update(index, role);
+    }
+
+    public void delete(int index) {
+        this.delete(index);
+    }
+
+    public void get(int index) {
+        this.get(index);
     }
 
 }
--- Part IV. Chapter 2. Generic/UserStore.java
+++ Part IV. Chapter 2. Generic/UserStore.java
@@ -2,9 +2,26 @@ package ru.shestakov.services;
 
 public class UserStore<User> {
 
-    SimpleArray[] sa;
+    SimpleArray<User> sa;
 
     public UserStore(int size) {
-        this.sa = new SimpleArray[size];
+        this.sa = new SimpleArray<User>(size);
     }
+
+    public void add(User user) {
+        this.sa.add(user);
+    }
+
+    public void update(int index, User user) {
+        this.sa.update(index, user);
+    }
+
+    public void delete(int index) {
+        this.delete(index);
+    }
+
+    public void get(int index) {
+        this.get(index);
+    }
+
 }
