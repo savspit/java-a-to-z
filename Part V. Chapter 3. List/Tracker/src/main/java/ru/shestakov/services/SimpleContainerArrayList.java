90e9cf626ba27c52109437a800759c438033845e
Anton Shestakov <savspit@gmail.com>
2016-06-10 16:07:00 +0300

    add structure with folder

--- Part V. Chapter 3. List/Tracker/src/main/java/ru/shestakov/services/SimpleContainerArrayList.java
+++ Part V. Chapter 3. List/Tracker/src/main/java/ru/shestakov/services/SimpleContainerArrayList.java
@@ -1,44 +1,76 @@
 package ru.shestakov.services;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.*;
 
-public class SimpleContainerArrayList<E> implements SimpleContainer<E> {
+public class SimpleContainerArrayList<E> {
 
-    private final ArrayList<E> values = new ArrayList<E>();
-    private int index = 0;
+    private Object[] elementData;
+    private int size;
 
-    @Override
-    public void add(E o) {
-        values.add(o);
+    public SimpleContainerArrayList() {
+        this(10);
     }
 
-    @Override
-    public void remove(E o) {
-        values.remove(o);
+    public SimpleContainerArrayList(int size) {
+        this.elementData = new Object[size];
     }
 
-    @Override
-    public int size() {
-        return values.size();
+    public boolean add(E e) {
+        ensureCapacityInternal(size + 1);
+        elementData[size++] = e;
+        return true;
     }
 
-    @Override
+    private void ensureCapacityInternal(int minCapacity) {
+        if(minCapacity > elementData.length) {
+            int newCapacity = elementData.length + (elementData.length >> 1);
+            elementData = Arrays.copyOf(elementData, newCapacity);
+        }
+    }
+
+    public boolean remove(Object o) {
+        for (int index = 0; index < size; index++)
+            if (o.equals(elementData[index])) {
+                fastRemove(index);
+                return true;
+            }
+        return false;
+    }
+
+    private void fastRemove(int index) {
+        int numMoved = size - index - 1;
+        if (numMoved > 0)
+            System.arraycopy(elementData, index+1, elementData, index,
+                    numMoved);
+        elementData[--size] = null;
+    }
+
     public E get(int index) {
-        return values.get(index);
+        return (E) elementData[index];
     }
 
-    @Override
-    public Iterator iterator() {
-        return null;
+    public int size() {
+        return size;
     }
 
-    public boolean hasNext() {
-        return values.size() > index;
+    public Iterator<E> iterator() {
+        return new Itr();
     }
 
-    public E next() {
-        return values.get(index++);
+    private class Itr implements Iterator<E> {
+        int cursor;
+
+        public boolean hasNext() {
+            return cursor != size;
+        }
+
+        public E next() {
+            return (E) elementData[cursor++];
+        }
+
+        public void remove() {
+
+        }
     }
 
 }
--- Part V. Chapter 3. List/Tracker/src/test/java/ru/shestakov/services/SimpleContainerArrayListTest.java
+++ Part V. Chapter 3. List/Tracker/src/test/java/ru/shestakov/services/SimpleContainerArrayListTest.java
@@ -2,6 +2,8 @@ package ru.shestakov.services;
 
 import org.junit.Test;
 
+import java.util.Iterator;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -26,4 +28,34 @@ public class SimpleContainerArrayListTest {
         assertThat(arrL.size(), is(0));
     }
 
+    @Test
+    public void whenLoopThenHaveCorrectCursor() {
+
+        SimpleContainerArrayList<Integer> arrL = new SimpleContainerArrayList<Integer>();
+        arrL.add(1);
+        arrL.add(2);
+
+        Integer count = 0;
+        Iterator iterator = arrL.iterator();
+        while (iterator.hasNext()) {
+            count = (Integer) iterator.next();
+        }
+
+        assertThat(arrL.get(1), is(count));
+    }
+
+    @Test
+    public void whenGrowSizeThenGrow() {
+
+        SimpleContainerArrayList<Integer> arrL = new SimpleContainerArrayList<Integer>(10);
+
+        int count = 0;
+        Iterator iterator = arrL.iterator();
+        while (iterator.hasNext()) {
+            iterator.next();
+            count++;
+        }
+
+        assertThat(arrL.size(), is(count));
+    }
 }
