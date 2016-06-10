30123454f3722c8e4ad80f127f56a77466eb73d0
Anton Shestakov <savspit@gmail.com>
2016-06-10 09:07:23 +0300

    add structure with folder

--- Part V. Chapter 2. Generic/Tracker/src/main/java/ru/shestakov/services/UserStore.java
+++ Part V. Chapter 2. Generic/Tracker/src/main/java/ru/shestakov/services/UserStore.java
@@ -6,23 +6,4 @@ public class UserStore<User> extends AbstractStore {
         super(size);
     }
 
-    @Override
-    public void add(Object o) {
-        super.add(o);
-    }
-
-    @Override
-    public void update(int index, Object o) {
-        super.update(index, o);
-    }
-
-    @Override
-    public void delete(int index) {
-        super.delete(index);
-    }
-
-    @Override
-    public void get(int index) {
-        super.get(index);
-    }
 }
