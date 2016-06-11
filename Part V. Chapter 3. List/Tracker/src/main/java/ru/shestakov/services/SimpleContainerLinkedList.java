a1f706c6588737f16aa8fab5c840c76bb2fc27c4
Anton Shestakov <savspit@gmail.com>
2016-06-11 20:21:35 +0300

    add structure with folder

--- Part V. Chapter 3. List/Tracker/src/main/java/ru/shestakov/services/SimpleContainerLinkedList.java
+++ Part V. Chapter 3. List/Tracker/src/main/java/ru/shestakov/services/SimpleContainerLinkedList.java
@@ -1,44 +1,131 @@
 package ru.shestakov.services;
 
-import java.util.LinkedList;
 import java.util.Iterator;
 
-public class SimpleContainerLinkedList<E> implements SimpleContainer<E> {
+public class SimpleContainerLinkedList<E> {
 
-    private final LinkedList<E> values = new LinkedList<E>();
-    private int index = 0;
+    public int size;
+    public Node<E> first;
+    public Node<E> last;
 
-    @Override
-    public void add(E o) {
-        values.add(o);
+    public SimpleContainerLinkedList() {
+        this.size = 0;
     }
 
-    @Override
-    public void remove(E o) {
-        values.remove(o);
+    public class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
     }
 
-    @Override
-    public int size() {
-        return values.size();
+    public boolean add(E e) {
+        linkLast(e);
+        return true;
     }
 
-    @Override
+    void linkLast(E e) {
+        Node<E> l = last;
+        Node<E> newNode = new Node<>(l, e, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        size++;
+    }
+
     public E get(int index) {
-        return values.get(index);
+        return node(index).item;
     }
 
-    @Override
-    public Iterator iterator() {
-        return null;
+    Node<E> node(int index) {
+
+        if (index < (size >> 1)) {
+            Node<E> x = first;
+            for (int i = 0; i < index; i++)
+                x = x.next;
+            return x;
+        } else {
+            Node<E> x = last;
+            for (int i = size - 1; i > index; i--)
+                x = x.prev;
+            return x;
+        }
     }
 
-    public boolean hasNext() {
-        return values.size() > index;
+    public int size() {
+        return size;
     }
 
-    public E next() {
-        return values.get(index++);
+    public boolean remove(Object o) {
+        for (Node<E> x = first; x != null; x = x.next) {
+            if (o.equals(x.item)) {
+                unlink(x);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    E unlink(Node<E> x) {
+
+        E element = x.item;
+        Node<E> next = x.next;
+        Node<E> prev = x.prev;
+
+        if (prev == null) {
+            first = next;
+        } else {
+            prev.next = next;
+            x.prev = null;
+        }
+
+        if (next == null) {
+            last = prev;
+        } else {
+            next.prev = prev;
+            x.next = null;
+        }
+
+        x.item = null;
+        size--;
+
+        return element;
+    }
+
+    public Iterator<E> iterator(int index) {
+        return new Itr(index);
+    }
+
+    private class Itr implements Iterator<E> {
+        private Node<E> lastReturned = null;
+        private Node<E> next;
+        private int nextIndex;
+
+        Itr(int index) {
+            next = (index == size) ? null : node(index);
+            nextIndex = index;
+        }
+
+        public boolean hasNext() {
+            return nextIndex < size;
+        }
+
+        public E next() {
+            lastReturned = next;
+            next = next.next;
+            nextIndex++;
+            return lastReturned.item;
+        }
+
+        public void remove() {
+        }
     }
 
 }
--- Part V. Chapter 3. List/Tracker/src/test/java/ru/shestakov/services/SimpleContainerLinkedListTest.java
+++ Part V. Chapter 3. List/Tracker/src/test/java/ru/shestakov/services/SimpleContainerLinkedListTest.java
@@ -2,6 +2,8 @@ package ru.shestakov.services;
 
 import org.junit.Test;
 
+import java.util.Iterator;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -19,11 +21,41 @@ public class SimpleContainerLinkedListTest {
     @Test
     public void whenRemoveThenContainerHaveNotValue() {
 
-        SimpleContainerArrayList<Integer> arrL = new SimpleContainerArrayList<Integer>();
+        SimpleContainerLinkedList<Integer> arrL = new SimpleContainerLinkedList<Integer>();
         arrL.add(1);
         arrL.remove(1);
 
         assertThat(arrL.size(), is(0));
     }
 
+    @Test
+    public void whenLoopThenHaveCorrectCursor() {
+
+        SimpleContainerLinkedList<Integer> arrL = new SimpleContainerLinkedList<Integer>();
+        arrL.add(1);
+        arrL.add(2);
+
+        Integer count = 0;
+        Iterator iterator = arrL.iterator(0);
+        while (iterator.hasNext()) {
+            count = (Integer) iterator.next();
+        }
+
+        assertThat(arrL.get(1), is(count));
+    }
+
+    @Test
+    public void whenGrowSizeThenGrow() {
+
+        SimpleContainerLinkedList<Integer> arrL = new SimpleContainerLinkedList<Integer>();
+
+        int count = 0;
+        Iterator iterator = arrL.iterator(0);
+        while (iterator.hasNext()) {
+            iterator.next();
+            count++;
+        }
+
+        assertThat(arrL.size(), is(count));
+    }
 }
